54f9759a0e230aec96ddbebad7d1856e8d42149b
Joshua <xjoshua2015x@gmail.com>
2026-01-18 14:52:54 +0000

    removed unused import

--- packages/tdd-components/src/components/AppTheme/customizations/inputs.tsx
+++ packages/tdd-components/src/components/AppTheme/customizations/inputs.tsx
@@ -7,7 +7,6 @@ import CheckBoxOutlineBlankRoundedIcon from "@mui/icons-material/CheckBoxOutline
 import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 import RemoveRoundedIcon from "@mui/icons-material/RemoveRounded";
 import { gray, brand } from "../themePrimitives";
-import { Opacity } from "@mui/icons-material";
 
 /* eslint-disable import/prefer-default-export */
 export const inputsCustomizations: Components<Theme> = {
